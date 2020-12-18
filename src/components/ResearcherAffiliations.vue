c4c0c0449d23a2e46571076d86ba8c4279fd1d5c
Wen Sun <syl1887415157@126.com>
2020-12-18 13:23:01 +0800

    feat(researcher): 增加历史机构、领域、论文页面

--- src/components/ResearcherAffiliations.vue
+++ src/components/ResearcherAffiliations.vue
@@ -1,16 +1,77 @@
 <template>
-  <p>历史机构：{{ id }}</p>
+  <div class="wrapper">
+    <el-card>
+      <template #header>
+        <div class="date-select">
+          <el-date-picker
+            v-model="startDate"
+            type="year"
+            placeholder="开始年份"
+            value-format="yyyy"
+          />
+          -
+          <el-date-picker
+            v-model="endDate"
+            type="year"
+            placeholder="结束年份"
+            value-format="yyyy"
+          />
+        </div>
+      </template>
+      <ul>
+        <li v-for="affiliationId of affiliationIds" :key="affiliationId">
+          {{ affiliationId }}
+        </li>
+      </ul>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Card, DatePicker } from "element-ui";
 
 export default Vue.extend({
   name: "ResearcherAffiliations",
   props: {
     id: String
+  },
+  components: {
+    [Card.name]: Card,
+    [DatePicker.name]: DatePicker
+  },
+  data() {
+    const currentYearStr = new Date().getFullYear().toString();
+    return {
+      startDate: currentYearStr,
+      endDate: currentYearStr,
+      affiliationIds: [] as string[]
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.affiliationIds = ["1", "2", "3", "4", "5"];
+    }, 500);
   }
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.wrapper {
+  min-height: 70vh;
+
+  .date-select {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .result-card {
+    margin-bottom: 40px;
+
+    .result-button {
+      float: right;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
--- src/components/ResearcherDomains.vue
+++ src/components/ResearcherDomains.vue
@@ -1,16 +1,77 @@
 <template>
-  <p>历史领域：{{ id }}</p>
+  <div class="wrapper">
+    <el-card>
+      <template #header>
+        <div class="date-select">
+          <el-date-picker
+            v-model="startDate"
+            type="year"
+            placeholder="开始年份"
+            value-format="yyyy"
+          />
+          -
+          <el-date-picker
+            v-model="endDate"
+            type="year"
+            placeholder="结束年份"
+            value-format="yyyy"
+          />
+        </div>
+      </template>
+      <ul>
+        <li v-for="domainId of domainIds" :key="domainId">
+          {{ domainId }}
+        </li>
+      </ul>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Card, DatePicker } from "element-ui";
 
 export default Vue.extend({
   name: "ResearcherDomains",
   props: {
     id: String
+  },
+  components: {
+    [Card.name]: Card,
+    [DatePicker.name]: DatePicker
+  },
+  data() {
+    const currentYearStr = new Date().getFullYear().toString();
+    return {
+      startDate: currentYearStr,
+      endDate: currentYearStr,
+      domainIds: [] as string[]
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.domainIds = ["1", "2", "3", "4", "5"];
+    }, 500);
   }
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.wrapper {
+  min-height: 70vh;
+
+  .date-select {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .result-card {
+    margin-bottom: 40px;
+
+    .result-button {
+      float: right;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
