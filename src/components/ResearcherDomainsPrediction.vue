123a317add9be1cd120fecd1aff51b331009c4f7
Wen Sun <syl1887415157@126.com>
2020-12-18 16:08:27 +0800

    feat(researcher): 增加领域预测页面

--- src/components/ResearcherDomainsPrediction.vue
+++ src/components/ResearcherDomainsPrediction.vue
@@ -1,16 +1,60 @@
 <template>
-  <p>领域预测：{{ id }}</p>
+  <div class="wrapper">
+    <el-card>
+      <template #header>
+        <h3>该学者未来可能的研究方向</h3>
+      </template>
+      <ul>
+        <li v-for="domainId of domainIds" :key="domainId">
+          {{ domainId }}
+        </li>
+      </ul>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Card, DatePicker } from "element-ui";
 
 export default Vue.extend({
   name: "ResearcherDomainsPrediction",
   props: {
     id: String
+  },
+  components: {
+    [Card.name]: Card,
+    [DatePicker.name]: DatePicker
+  },
+  data() {
+    return {
+      domainIds: [] as string[]
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.domainIds = ["1", "2", "3", "4", "5"];
+    }, 500);
   }
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.wrapper {
+  min-height: 70vh;
+
+  .date-select {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .result-card {
+    margin-bottom: 40px;
+
+    .result-button {
+      float: right;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
